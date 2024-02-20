97de52da6a5a8ab1a095d507021f799e75f9edc8
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2024-02-20 20:11:17 +0330

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <Navbar />
-          <main>{children}</main>
+          <main className="pl-5">{children}</main>
         </Theme>
       </body>
     </html>
